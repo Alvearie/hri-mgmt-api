8f5aefc98d1d5f0bc85aae3c733c56bb21b20414
Fred Ricci <60147759+FRicci618@users.noreply.github.com>
2021-09-03 14:29:09 -0400

    WHFHRI-664 (#6)
    Add a Github Actions workflow to hri-mgmt-api.
    
    Signed-off-by: fred <fjricci@us.ibm.com>

--- test/spec/hri_management_api_validation_spec.rb
+++ test/spec/hri_management_api_validation_spec.rb
@@ -9,8 +9,8 @@ describe 'HRI Management API With Validation' do
   INVALID_ID = 'INVALID'
   TENANT_ID = ENV['TENANT_ID']
   INTEGRATOR_ID = 'claims'
-  TEST_TENANT_ID = "rspec-#{ENV['TRAVIS_BRANCH'].delete('.')}-test-tenant".downcase
-  TEST_INTEGRATOR_ID = "rspec-#{ENV['TRAVIS_BRANCH'].delete('.')}-test-integrator".downcase
+  TEST_TENANT_ID = "rspec-#{ENV['BRANCH_NAME'].delete('.')}-test-tenant".downcase
+  TEST_INTEGRATOR_ID = "rspec-#{ENV['BRANCH_NAME'].delete('.')}-test-integrator".downcase
   DATA_TYPE = 'rspec-batch'
   STATUS = 'started'
   BATCH_INPUT_TOPIC = "ingest.#{TENANT_ID}.#{INTEGRATOR_ID}.in"
@@ -48,7 +48,7 @@ describe 'HRI Management API With Validation' do
     @kafka_consumer.subscribe("ingest.#{TENANT_ID}.#{INTEGRATOR_ID}.notification")
 
     #Create Batch
-    @batch_prefix = "rspec-#{ENV['TRAVIS_BRANCH'].delete('.')}"
+    @batch_prefix = "rspec-#{ENV['BRANCH_NAME'].delete('.')}"
     @batch_name = "#{@batch_prefix}-#{SecureRandom.uuid}"
     create_batch = {
       name: @batch_name,
@@ -93,7 +93,7 @@ describe 'HRI Management API With Validation' do
     end
 
     #Delete Batches
-    response = @elastic.es_delete_by_query(TENANT_ID, "name:rspec-#{ENV['TRAVIS_BRANCH']}*")
+    response = @elastic.es_delete_by_query(TENANT_ID, "name:rspec-#{ENV['BRANCH_NAME']}*")
     response.nil? ? (raise 'Elastic batch delete did not return a response') : (expect(response.code).to eq 200)
     Logger.new(STDOUT).info("Delete test batches by query response #{response.body}")
 
