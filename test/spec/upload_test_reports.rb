b253460a7569ec32da80a4703d7da55c13a6a7c5
fred <fjricci@us.ibm.com>
2021-09-15 17:05:10 -0400

    troubleshoot
    Signed-off-by: fred <fjricci@us.ibm.com>

--- test/spec/upload_test_reports.rb
+++ test/spec/upload_test_reports.rb
@@ -20,6 +20,7 @@ time = Time.now.strftime '%Y%m%d%H%M%S'
 if %w[main develop WHFHRI-667].include?(ENV['BRANCH_NAME'])
   if ARGV[0] == 'IVT'
     logger.info("Uploading ivttest-#{time}.xml to COS")
+    puts Dir.entries("#{__dir__}/../..")
     File.rename("#{__dir__}/../../ivttest.xml", "#{__dir__}/../../ivttest-#{time}.xml")
     cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "mgmt-api/#{ENV['BRANCH_NAME']}/ivt/ivttest-#{time}.xml", File.read(File.join(Dir.pwd, "ivttest-#{time}.xml")))
   elsif ARGV[0] == 'Dredd'
