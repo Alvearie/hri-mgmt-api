c11dc807806b6dfa6a60e78fdcfaa3e275e8e5ce
Fred Ricci <60147759+FRicci618@users.noreply.github.com>
2021-09-15 17:43:37 -0400

    Whfhri 667 (#12)
    * Install Allure and generate report
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Remove apt-add-repository
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Remove yml comments
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Only install Allure
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Install JRE
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Generate Allure Report for IVT
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Upload test reports to COS
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * troubleshoot filename
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Only upload ivttest.xml
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Fixed filename
    
    * list files before sed
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * fixed
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * troubleshoot
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * re-add missing steps
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * troubleshoot
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * combine test results
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Correct results file name
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * fixed
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Fix filename
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Fix file rename
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * troubleshoot
    
    Signed-off-by: fred <fjricci@us.ibm.com>
    
    * Call combine script first...
    
    Signed-off-by: fred <fjricci@us.ibm.com>

--- test/spec/upload_test_reports.rb
+++ test/spec/upload_test_reports.rb
@@ -6,28 +6,29 @@
 
 require_relative '../env'
 
-# This script uploads JUnit test reports to Cloud Object Storage to be used by the UnitTH application to generate HTML
-# test trend reports for the IVT and Dredd tests. More information on unitth can be found here: http://junitth.sourceforge.net/
+# This script uploads JUnit test reports to Cloud Object Storage to be used by the Allure application to generate HTML
+# test trend reports for the IVT and Dredd tests. More information on Allure can be found here: https://github.com/allure-framework/allure2
 #
-# The 'ivttest.xml' and 'dreddtests.xml' JUnit reports are uploaded to the 'hri-test-reports' Cloud Object Storage bucket,
-# which is also mounted on the 'unitth' kubernetes pod. This bucket keeps 30 days of reports that will be used to generate a
-# historical HTML report when the UnitTH jar is run on the pod.
+# The 'ivttest.xml' and 'dreddtests.xml' JUnit reports are uploaded to the 'wh-hri-dev1-allure-reports' Cloud Object Storage bucket,
+# which is also mounted on the 'allure' kubernetes pod. This bucket keeps 30 days of reports that will be used to generate a
+# historical HTML report when the allure executable is invoked on the pod.
 
 cos_helper = HRITestHelpers::COSHelper.new(ENV['COS_URL'], ENV['IAM_CLOUD_URL'], ENV['CLOUD_API_KEY'])
 logger = Logger.new(STDOUT)
 time = Time.now.strftime '%Y%m%d%H%M%S'
 
-if %w[main develop].include?(ENV['TRAVIS_BRANCH'])
+if %w[main develop WHFHRI-667].include?(ENV['BRANCH_NAME'])
   if ARGV[0] == 'IVT'
-    logger.info('Uploading ivttest.xml to COS')
-    `sed -i 's#test/ivt_test_results#rspec#g' ivttest.xml`
-    cos_helper.upload_object_data('wh-hri-dev1-test-reports', "mgmt-api/#{ENV['TRAVIS_BRANCH']}/ivt/#{time}/ivttest.xml", File.read(File.join(Dir.pwd, 'ivttest.xml')))
+    logger.info("Uploading ivttest-#{time}.xml to COS")
+    File.rename("#{Dir.pwd}/ivttest.xml", "#{Dir.pwd}/ivttest-#{time}.xml")
+    cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "mgmt-api/#{ENV['BRANCH_NAME']}/ivt/ivttest-#{time}.xml", File.read(File.join(Dir.pwd, "ivttest-#{time}.xml")))
   elsif ARGV[0] == 'Dredd'
-    logger.info('Uploading dreddtests.xml to COS')
-    cos_helper.upload_object_data('wh-hri-dev1-test-reports', "mgmt-api/#{ENV['TRAVIS_BRANCH']}/dredd/#{time}/dreddtests.xml", File.read(File.join(Dir.pwd, 'dreddtests.xml')))
+    logger.info("Uploading dreddtests-#{time}.xml to COS")
+    File.rename("#{Dir.pwd}/dreddtests.xml", "#{Dir.pwd}/dreddtests-#{time}.xml")
+    cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "mgmt-api/#{ENV['BRANCH_NAME']}/dredd/dreddtests-#{time}.xml", File.read(File.join(Dir.pwd, "dreddtests-#{time}.xml")))
   else
     raise "Invalid argument: #{ARGV[0]}. Valid arguments: 'IVT' or 'Dredd'"
   end
 else
-  logger.info("Test reports are only generated for the 'main' or 'develop' branches. Exiting.")
+  logger.info("Test reports are only generated for the 'main', 'develop', or 'WHFHRI-667' branches. Exiting.")
 end
