225ca4fc67e85b98d250956ab108712a1395488f
fred <fjricci@us.ibm.com>
2021-09-15 16:43:53 -0400

    Fix file rename
    Signed-off-by: fred <fjricci@us.ibm.com>

--- test/spec/upload_test_reports.rb
+++ test/spec/upload_test_reports.rb
@@ -20,11 +20,11 @@ time = Time.now.strftime '%Y%m%d%H%M%S'
 if %w[main develop WHFHRI-667].include?(ENV['BRANCH_NAME'])
   if ARGV[0] == 'IVT'
     logger.info("Uploading ivttest-#{time}.xml to COS")
-    File.rename("#{Dir.pwd}/ivttest.xml", "#{Dir.pwd}/ivttest-#{time}.xml")
+    File.rename("#{__dir__}/../../ivttest.xml", "#{__dir__}/../../ivttest-#{time}.xml")
     cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "mgmt-api/#{ENV['BRANCH_NAME']}/ivt/ivttest-#{time}.xml", File.read(File.join(Dir.pwd, "ivttest-#{time}.xml")))
   elsif ARGV[0] == 'Dredd'
     logger.info("Uploading dreddtests-#{time}.xml to COS")
-    File.rename("#{Dir.pwd}/dreddtests.xml", "#{Dir.pwd}/dreddtests-#{time}.xml")
+    File.rename("#{__dir__}/../../dreddtests.xml", "#{__dir__}/../../dreddtests-#{time}.xml")
     cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "mgmt-api/#{ENV['BRANCH_NAME']}/dredd/dreddtests-#{time}.xml", File.read(File.join(Dir.pwd, "dreddtests-#{time}.xml")))
   else
     raise "Invalid argument: #{ARGV[0]}. Valid arguments: 'IVT' or 'Dredd'"
