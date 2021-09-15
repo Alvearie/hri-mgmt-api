795e322cb19e19a153db04bfbef864240587b7eb
fred <fjricci@us.ibm.com>
2021-09-15 11:43:18 -0400

    list files before sed
    Signed-off-by: fred <fjricci@us.ibm.com>

--- test/spec/upload_test_reports.rb
+++ test/spec/upload_test_reports.rb
@@ -20,6 +20,9 @@ time = Time.now.strftime '%Y%m%d%H%M%S'
 if %w[main develop WHFHRI-667].include?(ENV['BRANCH_NAME'])
   if ARGV[0] == 'IVT'
     logger.info('Uploading ivttest.xml to COS')
+    `ls`
+    echo '-------------'
+    `ls test`
     `sed -i 's#test/ivt_test_results#rspec#g' ivttest-#{time}.xml`
     cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "mgmt-api/#{ENV['BRANCH_NAME']}/ivt/ivttest-#{time}.xml", File.read(File.join(Dir.pwd, "ivttest-#{time}.xml")))
   elsif ARGV[0] == 'Dredd'
