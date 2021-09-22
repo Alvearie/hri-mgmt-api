17f676afa3474eed9351bdea0e475293f19d9bc3
Dan Baxter <djbaxter@us.ibm.com>
2021-09-22 11:55:05 -0500

    WHFHRI-691: Add copyright check.
    Signed-off-by: Dan Baxter <djbaxter@us.ibm.com>

---
+++ copyrightCheck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# (C) Copyright IBM Corp. 2020
+#
+# SPDX-License-Identifier: Apache-2.0
+
+files=$(find . -name "*.go" -or -name "*.rb" -or -name "*.sh")
+
+rtn=0
+for file in $files; do
+  #echo $file
+  if ! head -n 5 $file | grep -qE '(Copyright IBM Corp)|(MockGen)'; then
+   rtn=1
+   echo $file
+  fi
+done
+
+if [ $rtn -ne 0 ]; then
+  echo "Found files without copyright, exiting."
+  exit 1
+fi
