fe7faaa53054cd66d1ab8c412947f2598a98d43a
Fred Ricci <60147759+FRicci618@users.noreply.github.com>
2021-09-24 16:16:48 -0400

    Whfhri 668 support 2.x (#23)
    * Upload test reports
    
    Signed-off-by: fred <fjricci@us.ibm.com>

--- test/spec/cos_helper.rb
+++ test/spec/cos_helper.rb
@@ -10,12 +10,6 @@ class COSHelper
     @iam_token = IAMHelper.new.get_access_token
   end
 
-  def get_object_data(bucket_name, object_path)
-    #Get COS object, or use local data if object does not exist
-    response = @helper.rest_get("#{@cos_url}/#{bucket_name}/#{object_path}", {'Authorization' => "Bearer #{@iam_token}"})
-    response.code == 200 ? response.body : File.read(File.join(File.dirname(__FILE__), 'fhir_practitioners.txt'))
-  end
-
   def upload_object_data(bucket_name, object_path, object)
     response = @helper.rest_put("#{@cos_url}/#{bucket_name}/#{object_path}", object, {'Authorization' => "Bearer #{@iam_token}"})
     raise 'Failed to upload object to COS' unless response.code == 200
---
+++ test/spec/upload_test_reports.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+# (C) Copyright IBM Corp. 2021
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require_relative '../env'
+
+# This script uploads JUnit test reports to Cloud Object Storage to be used by the Allure application to generate HTML
+# test trend reports for the IVT and Dredd tests. More information on Allure can be found here: https://github.com/allure-framework/allure2
+#
+# The 'ivttest.xml' and 'dreddtests.xml' JUnit reports are uploaded to the 'wh-hri-dev1-allure-reports' Cloud Object Storage bucket,
+# which is also mounted on the 'allure' kubernetes pod. This bucket keeps 30 days of reports that will be used to generate a
+# historical HTML report when the allure executable is invoked on the pod.
+
+cos_helper = COSHelper.new
+logger = Logger.new(STDOUT)
+time = Time.now.strftime '%Y%m%d%H%M%S'
+
+if ARGV[0] == 'IVT'
+  logger.info("Uploading ivttest-#{time}.xml to COS")
+  File.rename("#{Dir.pwd}/ivttest.xml", "#{Dir.pwd}/hri-mgmt-api-ivttest-#{time}.xml")
+  cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "hri-mgmt-api-ivttest-#{time}.xml", File.read(File.join(Dir.pwd, "hri-mgmt-api-ivttest-#{time}.xml")))
+elsif ARGV[0] == 'Dredd'
+  logger.info("Uploading dreddtests-#{time}.xml to COS")
+  doc = Nokogiri::XML(File.open("#{Dir.pwd}/dreddtests.xml")) { |file| file.noblanks }
+  doc.search('//testsuite').attribute('name').value = "hri-mgmt-api - #{ENV['BRANCH_NAME']} - Dredd"
+
+  #Dredd XUnit output contains skipped tests, so delete these elements from the result xml
+  doc.search('//testsuite/testcase').each do |block|
+    block.remove if block.children.count == 1
+  end
+
+  File.write("#{Dir.pwd}/dreddtests.xml", doc)
+  File.rename("#{Dir.pwd}/dreddtests.xml", "#{Dir.pwd}/hri-mgmt-api-dreddtests-#{time}.xml")
+  cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "hri-mgmt-api-dreddtests-#{time}.xml", File.read(File.join(Dir.pwd, "hri-mgmt-api-dreddtests-#{time}.xml")))
+else
+  raise "Invalid argument: #{ARGV[0]}. Valid arguments: 'IVT' or 'Dredd'"
+end
