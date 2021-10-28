ebdacc14e61a15bda4c98f1033c38ed35922a9f3
Fred Ricci <60147759+FRicci618@users.noreply.github.com>
2021-10-28 10:36:56 -0400

    Whfhri 730 (#52)
    * Fix docker build toolchain ID, fix IVT, resolve Nokogiri dependency
    
    Signed-off-by: Fred Ricci <60147759+FRicci618@users.noreply.github.com>

--- test/spec/hri_management_api_no_validation_spec.rb
+++ test/spec/hri_management_api_no_validation_spec.rb
@@ -437,6 +437,12 @@ describe 'HRI Management API Without Validation' do
     it 'Success With Invalid Topic Only' do
       invalid_topic = "ingest.#{TENANT_ID}.#{TEST_INTEGRATOR_ID}.invalid"
       @event_streams_helper.create_topic(invalid_topic, 1)
+      Timeout.timeout(30, nil, "Timed out waiting for the '#{invalid_topic}' topic to be created") do
+        loop do
+          break if @event_streams_helper.get_topics.include?(invalid_topic)
+        end
+      end
+
       response = @mgmt_api_helper.hri_get_tenant_streams(TENANT_ID)
       expect(response.code).to eq 200
       parsed_response = JSON.parse(response.body)
@@ -446,12 +452,9 @@ describe 'HRI Management API Without Validation' do
       end
       raise "Tenant Stream Not Found: #{TEST_INTEGRATOR_ID}" unless stream_found
 
-      Timeout.timeout(15, nil, "Timed out waiting for the '#{invalid_topic}' topic to be deleted") do
+      @event_streams_helper.delete_topic(invalid_topic)
+      Timeout.timeout(30, nil, "Timed out waiting for the '#{invalid_topic}' topic to be deleted") do
         loop do
-          break if @event_streams_helper.get_topics.include?(invalid_topic)
-        end
-        loop do
-          @event_streams_helper.delete_topic(invalid_topic)
           break unless @event_streams_helper.get_topics.include?(invalid_topic)
         end
       end
@@ -612,16 +615,6 @@ describe 'HRI Management API Without Validation' do
     end
 
     it 'should auto-delete a batch from Elastic if the batch was created with an invalid Kafka topic' do
-      #Gather existing batches
-      existing_batches = []
-      response = @mgmt_api_helper.hri_get_batches(TENANT_ID, nil, { 'Authorization' => "Bearer #{@token_all_roles}" })
-      expect(response.code).to eq 200
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response['total']).to be > 0
-      parsed_response['results'].each do |batch|
-        existing_batches << batch['id'] unless batch['dataType'] == 'rspec-batch'
-      end
-
       #Create Batch with Bad Topic
       @batch_template[:topic] = 'INVALID-TEST-TOPIC'
       @batch_template[:dataType] = 'rspec-invalid-batch'
@@ -631,23 +624,19 @@ describe 'HRI Management API Without Validation' do
       expect(parsed_response['errorDescription']).to eql 'kafka producer error: Broker: Unknown topic or partition'
 
       #Verify Batch Delete
-      50.times do
-        new_batches = []
-        @batch_deleted = false
-        response = @mgmt_api_helper.hri_get_batches(TENANT_ID, nil, { 'Authorization' => "Bearer #{@token_all_roles}" })
-        expect(response.code).to eq 200
-        parsed_response = JSON.parse(response.body)
-        expect(parsed_response['total']).to be > 0
-        parsed_response['results'].each do |batch|
-          new_batches << batch['id'] unless batch['dataType'] == 'rspec-batch'
-        end
-        if existing_batches.sort == new_batches.sort
-          @batch_deleted = true
-          break
+      Timeout.timeout(30, nil, 'Batch with invalid topic not deleted after 30 seconds') do
+        loop do
+          response = @mgmt_api_helper.hri_get_batches(TENANT_ID, nil, { 'Authorization' => "Bearer #{@token_all_roles}" })
+          expect(response.code).to eq 200
+          parsed_response = JSON.parse(response.body)
+          expect(parsed_response['total']).to be > 0
+          @batch_found = false
+          parsed_response['results'].each do |batch|
+            @batch_found = true if batch['dataType'] == 'rspec-invalid-batch'
+          end
+          break unless @batch_found
         end
       end
-
-      expect(@batch_deleted).to be true
     end
 
     it 'Invalid Name' do
