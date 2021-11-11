d1bd390a9ac4788bf672113df08a52b329d7497f
Fred Ricci <60147759+FRicci618@users.noreply.github.com>
2021-11-11 12:49:37 -0500

    Upload the latest test report only
    Signed-off-by: Fred Ricci <60147759+FRicci618@users.noreply.github.com>

--- test/spec/upload_test_reports.rb
+++ test/spec/upload_test_reports.rb
@@ -10,19 +10,18 @@ require_relative '../env'
 # test trend reports for the IVT and Dredd tests. More information on Allure can be found here: https://github.com/allure-framework/allure2
 #
 # The 'ivttest.xml' and 'dreddtests.xml' JUnit reports are uploaded to the 'wh-hri-dev1-allure-reports' Cloud Object Storage bucket,
-# which is also mounted on the 'allure' kubernetes pod. This bucket keeps 30 days of reports that will be used to generate a
-# historical HTML report when the allure executable is invoked on the pod.
+# which is also mounted on the 'allure' kubernetes pod. This bucket keeps the latest test report that will be used to generate a
+# test dashboard when the allure executable is invoked on the pod.
 
 cos_helper = COSHelper.new
 logger = Logger.new(STDOUT)
-time = Time.now.strftime '%Y%m%d%H%M%S'
 
 if ARGV[0] == 'IVT'
-  logger.info("Uploading ivttest-#{time}.xml to COS")
-  File.rename("#{Dir.pwd}/ivttest.xml", "#{Dir.pwd}/hri-mgmt-api-ivttest-#{time}.xml")
-  cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "hri-mgmt-api-ivttest-#{time}.xml", File.read(File.join(Dir.pwd, "hri-mgmt-api-ivttest-#{time}.xml")))
+  logger.info("Uploading ivttest-#{ENV['BRANCH_NAME']}.xml to COS")
+  File.rename("#{Dir.pwd}/ivttest.xml", "#{Dir.pwd}/hri-mgmt-api-ivttest-#{ENV['BRANCH_NAME']}.xml")
+  cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "hri-mgmt-api-ivttest-#{ENV['BRANCH_NAME']}.xml", File.read(File.join(Dir.pwd, "hri-mgmt-api-ivttest-#{ENV['BRANCH_NAME']}.xml")))
 elsif ARGV[0] == 'Dredd'
-  logger.info("Uploading dreddtests-#{time}.xml to COS")
+  logger.info("Uploading dreddtests-#{ENV['BRANCH_NAME']}.xml to COS")
   doc = Nokogiri::XML(File.open("#{Dir.pwd}/dreddtests.xml")) { |file| file.noblanks }
   doc.search('//testsuite').attribute('name').value = "hri-mgmt-api - #{ENV['BRANCH_NAME']} - Dredd"
 
@@ -32,8 +31,8 @@ elsif ARGV[0] == 'Dredd'
   end
 
   File.write("#{Dir.pwd}/dreddtests.xml", doc)
-  File.rename("#{Dir.pwd}/dreddtests.xml", "#{Dir.pwd}/hri-mgmt-api-dreddtests-#{time}.xml")
-  cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "hri-mgmt-api-dreddtests-#{time}.xml", File.read(File.join(Dir.pwd, "hri-mgmt-api-dreddtests-#{time}.xml")))
+  File.rename("#{Dir.pwd}/dreddtests.xml", "#{Dir.pwd}/hri-mgmt-api-dreddtests-#{ENV['BRANCH_NAME']}.xml")
+  cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "hri-mgmt-api-dreddtests-#{ENV['BRANCH_NAME']}.xml", File.read(File.join(Dir.pwd, "hri-mgmt-api-dreddtests-#{ENV['BRANCH_NAME']}.xml")))
 else
   raise "Invalid argument: #{ARGV[0]}. Valid arguments: 'IVT' or 'Dredd'"
 end
